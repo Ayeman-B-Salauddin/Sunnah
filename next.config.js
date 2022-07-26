9f6ee227fde1f46c794e342f1ee9effb25300eee
Ayeman <ayemanbsalauddin@gmail.com>
2022-07-26 18:32:28 +0600

    Preact Build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPreact = require("next-plugin-preact");
+
+module.exports = withPreact({
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+});
